0e492043406d2b9f5aa50fb17a5218ddb4f44b86
Tobias Johansson <tobias@johansson.xyz>
2016-02-12 19:35:55 +0100

    Add view skeleton

--- js/app.js
+++ js/app.js
@@ -5,4 +5,8 @@ $(function() {
 	//And create the needed controllers and views
 	var exampleView = new ExampleView($("#exampleView"), model);
 
+	$("#homeButton").click(function(){
+		$("#homeView").toggleClass('hidden');
+		$("#dinnerView").toggleClass('hidden');
+	});
 });
