17598ac184027e734e18ddc8a2eb88d99d266c1d
Daniel Delgado <d_delgado89@hotmail.com>
2016-02-16 16:26:50 +0100

    small modifications to text

--- js/view/detailsView.js
+++ js/view/detailsView.js
@@ -3,11 +3,11 @@ var DetailsView = function(container, model) {
 	this.overview = container.find('#detailsOverview');
 	this.ingredients = container.find('#detailsIngredients');
 
-	var dish = model.getDish(100);
+	var dish = model.getDish(200);
 
 	//this.name.html(dish.name);
 	this.overview.html('<h1>' + dish.name.toUpperCase() + '</h1>' + '<img src="images/' + dish.image + '" width="100%">' + model.getDummyText());
-	this.ingredients.html('<h3>INGREDIENTS FOR 4 PEOPLE</h3><hr>' + getIngredientListHTML(dish.ingredients) + '<p>ssssssssssssss</p>');
+	this.ingredients.html('<h3>INGREDIENTS FOR 4 PEOPLE</h3><hr>' + getIngredientListHTML(dish.ingredients)) ;
 }
 
 var getIngredientListHTML = function(ingredients) {
@@ -20,10 +20,10 @@ var getIngredientListHTML = function(ingredients) {
 
 var getIngredientHTML = function(ingredient) {
 	return '' +
-	'<div class="col-md-2">' +
+	'<div class="col-md-3">' +
 		ingredient.quantity + ' ' + ingredient.unit +
 	'</div>' +
-	'<div class="col-md-6">' +
+	'<div class="col-md-5">' +
 		ingredient.name +
 	'</div>' +
 	'<div class="col-md-2">' +
