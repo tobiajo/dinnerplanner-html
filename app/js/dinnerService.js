addd8e49884db3c3d84723324ff5dab995ec7bfb
Oscar Eriksson <mikeandbob@gmail.com>
2016-03-08 18:55:08 +0100

    model interface

--- app/js/dinnerService.js
+++ app/js/dinnerService.js
@@ -4,28 +4,63 @@
 // service is created first time it is needed and then just reuse it
 // the next time.
 dinnerPlannerApp.factory('Dinner',function ($resource) {
-  
-  var numberOfGuest = 4;
+    var menu = [];
 
-  var menu = ['dish1','dish2','dish3'];
-
-
-  this.setNumberOfGuests = function(num) {
-    if (num >= 1) {
-      numberOfGuest = num;
+    var numberOfGuest = 4;
+    
+    this.getFullMenu = function() {
+        return menu;
     }
-  }
 
-  this.getNumberOfGuests = function() {
-    return numberOfGuest;
-  }
+    this.getDishPrice = function(dish) {
+        var priceOut = 0;
+        for (var i = 0; i < dish.ingredients.length; i++) {
+            priceOut += dish.ingredients[i].Quantity;
+        }
+        return priceOut; 
+    }
 
-  this.getFullMenu = function() {
-    return menu;
-  }
+    this.getAllIngredients = function () {
+        var ingredientsOut = [];
+        for (var i = 0; i < menu.length; i++) {
+            if (menu[i] != null) {
+                ingredientsOut = ingredientsOut.concat(menu[i].Ingredients);
+            }
+        }
+        return ingredientsOut;
+    }
+
+    this.getTotalMenuPrice = function() {
+        var priceOut = 0;
+        var ingredients = this.getAllIngredients();
+        for (var i = 0; i < ingredients.length; i++) {
+            priceOut += ingredients[i].Quantity;
+        }
+        return priceOut * this.getNumberOfGuests();    
+    }
+
+    this.addDishToMenu = function(dish) {
+        menu.push(dish);
+    } 
+    
+
+
+    this.setNumberOfGuests = function(num) {
+        if (num >= 1) {
+        numberOfGuest = num;
+        }
+    }
+
+    this.getNumberOfGuests = function() {
+        return numberOfGuest;
+    }
+
+    this.getFullMenu = function() {
+        return menu;
+    }
   
-  this.DishSearch = $resource('http://api.bigoven.com/recipes',{pg:1,rpp:25,api_key:'r02x0R09O76JMCMc4nuM0PJXawUHpBUL'});
-  this.Dish = $resource('http://api.bigoven.com/recipe/:id',{api_key:'r02x0R09O76JMCMc4nuM0PJXawUHpBUL'}); 
+    this.DishSearch = $resource('http://api.bigoven.com/recipes',{pg:1,rpp:25,api_key:'r02x0R09O76JMCMc4nuM0PJXawUHpBUL'});
+    this.Dish = $resource('http://api.bigoven.com/recipe/:id',{api_key:'r02x0R09O76JMCMc4nuM0PJXawUHpBUL'}); 
 
   // TODO in Lab 5: Add your model code from previous labs
   // feel free to remove above example code
@@ -41,6 +76,6 @@ dinnerPlannerApp.factory('Dinner',function ($resource) {
   // methods created in it. You can consider that this is instead
   // of calling var model = new DinnerModel() we did in the previous labs
   // This is because Angular takes care of creating it when needed.
-  return this;
+    return this;
 
 });
