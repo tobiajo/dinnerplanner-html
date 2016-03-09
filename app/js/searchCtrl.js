b18ff05ce17527fe42e90b6a68a734dd3f71632e
Oscar Eriksson <mikeandbob@gmail.com>
2016-03-09 09:50:06 +0100

    removed some printouts

--- app/js/searchCtrl.js
+++ app/js/searchCtrl.js
@@ -4,10 +4,8 @@ dinnerPlannerApp.controller('SearchCtrl', function ($scope,Dinner) {
     $scope.status = null;
 
     $scope.search = function(query) {
-        console.log(query);
         $scope.status = "Searching...";
         Dinner.DishSearch.get({title_kw:query},function(data){
-            console.log(data);
             $scope.dishes=data.Results;
             $scope.status = "Showing " + data.Results.length + " results";
         },function(data){
